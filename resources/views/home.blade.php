aaecf6974be9cb23cd911c1ee13fce7c96b70105
RoberFdez <roberto.fernandez.work@gmail.com>
2020-10-07 12:51:36 -0500

    Laravel Auth / Adminlte

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Notaria 2')
+
+@section('content_header')
+    <h1>Notaria 2</h1>
+@stop
+
+@section('content')
+    <p>Welcome</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
